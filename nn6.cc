a0a7230213e63a8bb596e71f64fded8888c8279d
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2015-08-28 16:58:22 +0200

    Command-line processing for nn6.

--- nn6.cc
+++ nn6.cc
@@ -4,6 +4,8 @@
 #include <limits>
 #include <vector>
 
+#include <boost/lexical_cast.hpp>
+
 #include "nn6.h"
 #include "nnopt.h"
 
@@ -22,17 +24,25 @@ auto precision_recall(const Eigen::MatrixBase<Derived1> &pred, const Eigen::Matr
 	return pr;
 }
 
-int main() {
+int main(int argc, char **argv) {
+	if(argc != 7) {
+		std::cerr << "Usage: nn6 train.nn6 val.nn6 U antembed srcembed hidden" << std::endl;
+		return 1;
+	}
+	std::string train_nn6 = argv[1];
+	std::string val_nn6 = argv[2];
+	std::size_t size_U = boost::lexical_cast<std::size_t>(argv[3]);
+	std::size_t size_antembed = boost::lexical_cast<std::size_t>(argv[4]);
+	std::size_t size_srcembed = boost::lexical_cast<std::size_t>(argv[5]);
+	std::size_t size_hidden = boost::lexical_cast<std::size_t>(argv[6]);
+
 	nn6::vocmap srcvocmap;
 	nn6::vocmap antvocmap;
-	//auto train = nn6::load_nn6<double>("/work/users/chm/ncv9.train.nn6", srcvocmap, antvocmap);
-	auto train = nn6::load_nn6<double>("/work/users/chm/ncv9.smalltrain.nn6", srcvocmap, antvocmap);
-	auto val = nn6::load_nn6<double>("/work/users/chm/ncv9.val.nn6", srcvocmap, antvocmap, train.nlink());
-	//auto train = nn6::load_nn6<double>("/work/users/chm/ncv9.1ex.nn6", srcvocmap, antvocmap);
-	//auto val = nn6::load_nn6<double>("/work/users/chm/ncv9.1ex.nn6", srcvocmap, antvocmap);
+	auto train = nn6::load_nn6<double>(train_nn6, srcvocmap, antvocmap);
+	auto val = nn6::load_nn6<double>(val_nn6, srcvocmap, antvocmap, train.nlink());
 	std::cerr << "Data loaded." << std::endl;
 
-	auto net = nn6::make_nn6<double>(train.input(), 20, 20, 20, 50);
+	auto net = nn6::make_nn6<double>(train.input(), size_U, size_antembed, size_srcembed, size_hidden);
 	typedef decltype(net) net_type;
 
 	nnet::nnopt<net_type> opt(net);
