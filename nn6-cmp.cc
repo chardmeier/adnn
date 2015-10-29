d601a914d4435fc0bae593f68d2ec1f653eea7c5
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2015-10-29 14:34:11 +0100

    Output all relevant data in nn6-cmp.

--- nn6-cmp.cc
+++ nn6-cmp.cc
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <fstream>
 #include <functional>
 #include <iostream>
 #include <limits>
@@ -10,6 +11,28 @@
 #include "nn6.h"
 #include "nnopt.h"
 
+template<class Idx,class Seq>
+void join_and_dump(const Seq &sequence, const char *outfile) {
+	typedef typename boost::mpl::push_back<Idx,boost::mpl::int_<0>>::type WIdx;
+	typedef typename boost::mpl::push_back<Idx,boost::mpl::int_<1>>::type BIdx;
+	const auto &weights = netops::at_spec<WIdx>(sequence);
+	const auto &bias = netops::at_spec<BIdx>(sequence);
+	Eigen::MatrixXd concat(weights.rows() + 1, weights.cols());
+	concat << bias, weights;
+	std::ofstream os(outfile);
+	os << concat << '\n';
+}
+
+template<class Weights>
+void dump_weights(const Weights &ww) {
+	join_and_dump<nn6::idx::W_U>(ww.sequence(), "weights.linkAhid");
+	join_and_dump<nn6::idx::W_V>(ww.sequence(), "weights.AhidAres");
+	join_and_dump<nn6::idx::W_antembed>(ww.sequence(), "weights.antembed");
+	join_and_dump<nn6::idx::W_srcembed>(ww.sequence(), "weights.srcembed");
+	join_and_dump<nn6::idx::W_embhid>(ww.sequence(), "weights.embhid");
+	join_and_dump<nn6::idx::W_hidout>(ww.sequence(), "weights.hidout");
+}
+
 int main(int argc, char **argv) {
 	if(argc != 2) {
 		std::cerr << "Usage: nn6 input.nn6" << std::endl;
@@ -18,12 +41,10 @@ int main(int argc, char **argv) {
 
 	std::string datafile = argv[1];
 
-/*
 	std::size_t size_U = 20;
 	std::size_t size_antembed = 20;
 	std::size_t size_srcembed = 20;
 	std::size_t size_hidden = 50;
-*/
 
 	nn6::classmap classmap("/cluster/home/chm/adnn/cmpex/5class.classes", true);
 	nn6::tagmap tagmap("/usit/abel/u1/chm/WMT2013.en-fr/pronoun-model/lefff-2.1.utf8.tags");
@@ -34,27 +55,20 @@ int main(int argc, char **argv) {
 
 	nn6::dump_nn6_dataset("dump", dataset, srcvocmap, antvocmap);
 
-/*
-	auto net = nn6::make_nn6<double>(train.input(),
-		size_U, size_antembed, size_srcembed, size_hidden, train.nclasses(),
-		params.get<double>("nn6.dropout-src", 1.0));
+	auto net = nn6::make_nn6<double>(dataset.input(),
+		size_U, size_antembed, size_srcembed, size_hidden, dataset.nclasses(),
+		1.0);
 	typedef decltype(net) net_type;
 
-	nnet::nnopt<net_type> opt(net, params.get_child("nn6.nnopt"));
-	nnet::nnopt_results<net_type> res = opt.train(net, train, val);
+	net_type::weight_type ww(net.spec());
+	ww.init_normal(.4);
 
-	std::cerr << "Training error: ";
-	std::copy(res.trainerr.begin(), res.trainerr.end(), std::ostream_iterator<net_type::float_type>(std::cerr, " "));
-	std::cerr << "\nValidation error: ";
-	std::copy(res.valerr.begin(), res.valerr.end(), std::ostream_iterator<net_type::float_type>(std::cerr, " "));
-	std::cerr << std::endl;
+	dump_weights(ww);
 
-	auto testout = net(res.best_weights, testset.sequence());
-	std::cerr << "Test error: " << net.error(testout, testset.targets()) << '\n';
-	std::cerr << "Precision/recall:\n" << precision_recall(testout.matrix(), testset.targets().matrix()) << std::endl;
+	auto output = net(ww, dataset.sequence());
 
-	std::cout << res.best_weights << std::endl;
-*/
+	std::ofstream output_os("output");
+	output_os << output;
 
 	return 0;
 }
